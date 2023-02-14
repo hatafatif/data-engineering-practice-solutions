c1ddeebb6b516c38c01904e9c0cf753790b91fb1
Hataf Atif <hatafbinatif@gmail.com>
2023-02-14 18:47:36 +0500

    Exercise-1 working solution

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,5 +1,9 @@
 import requests
 
+from zipfile import ZipFile, error
+from os import mkdir, getcwd, listdir, remove
+
+
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip',
@@ -10,10 +14,50 @@ download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip'
 ]
 
+def extract_filename_from_uri(link):
+    """
+    Input: URI
+    Output: Filename (Expected to be after the last appearing "/" in the URI)
+    """
+    seperator = 0
+    for n, char in enumerate(link[::-1]):
+        if char == "/": #This would be the last occuring /
+            seperator = n
+            break
+    break_from = len(link) - seperator
+    filename = link[break_from:]
+    return(filename)
+
 
 def main():
-    # your code here
-    pass
+    print('Start of program')
+    # Create 'downloads' folder if not present.
+    pwd = getcwd()
+    if 'downloads' not in listdir(pwd):
+        mkdir(pwd + '/downloads')
+
+    # Download all links 
+    for link in download_uris:
+
+    # Download files one by one, renaming them to the URI name using helper function
+        filename = extract_filename_from_uri(link)
+        print(f'Requesting file: {filename}')
+        r = requests.get(link, allow_redirects=True)
+        open(f"downloads/{filename}", 'wb').write(r.content)
+
+    # Extract file from zip file
+        print(f'Unzipping file: {filename}')
+        try:
+            with ZipFile(f"downloads/{filename}", mode='r') as archive:
+                for file in archive.namelist():
+                    # If file name is not csv or file name is in a subdirectory, ignore.
+                    if file.split(".")[-1] == "csv" and "/" not in file:
+                        archive.extract(file, path="downloads/")
+        except error:
+            print("Unzipping failed")
+
+    # Remove zip file
+        remove(f"downloads/{filename}") 
 
 
 if __name__ == '__main__':
